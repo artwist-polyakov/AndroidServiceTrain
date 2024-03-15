49532374654a0ad6ed084377494720557b68c9e2
Aleksandr Poliakov <master@artwist.ru>
2024-03-15 22:36:54 +0300

    выносит работу прогресс сервиса в фоновый поток

--- app/src/main/java/com/awst/androidservicetrain/ProgressService.kt
+++ app/src/main/java/com/awst/androidservicetrain/ProgressService.kt
@@ -4,26 +4,29 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import java.util.concurrent.Executors
 
 internal class ProgressService : Service() {
 
+    private companion object {
+        const val LOG_TAG = "ProgressService"
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        for (i in 0..<100) {
-            Log.d(LOG_TAG, "Progress: ${i}%")
-            Thread.sleep(2000)
+        // Запускает новый поток
+        Executors.newSingleThreadExecutor().execute {
+            for (i in 0..<100) {
+                Log.d(LOG_TAG, "Progress: ${i}%")
+                Thread.sleep(2000)
+            }
+            stopSelf()
         }
-        stopSelf()
 
         return START_NOT_STICKY
-    }
-
-    private companion object {
-        const val LOG_TAG = "ProgressService"
     }
 
 }
