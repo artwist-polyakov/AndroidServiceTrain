503c83ac35dc807d5db5f03f5812b5cdbe8979d9
Aleksandr Poliakov <master@artwist.ru>
2024-03-15 22:06:14 +0300

    подключает вызов сервиса на main activity

--- app/src/main/java/com/awst/androidservicetrain/MainActivity.kt
+++ app/src/main/java/com/awst/androidservicetrain/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.awst.androidservicetrain
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        findViewById<Button>(R.id.startMusicServiceButton).setOnClickListener {
+            // Создаём intent для запуска сервиса
+            val intent = Intent(this, MusicService::class.java).apply {
+                putExtra("song_url", SONG_URL)
+            }
+
+            // Стартуем сервис
+            startService(intent)
         }
     }
+
+    private companion object {
+        const val SONG_URL = "https://rus.hitmotop.com/get/music/20191128/Lenka_-_Blue_Skies_Revoke_Remix_67448680.mp3"
+    }
+
 }
