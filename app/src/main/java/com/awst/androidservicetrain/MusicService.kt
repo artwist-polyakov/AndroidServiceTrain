16d271254e8540aa61d6d692c9e7e9a003608df3
Aleksandr Poliakov <master@artwist.ru>
2024-03-15 22:12:00 +0300

    переопределяет onCreate и onDestroy для сервиса

--- app/src/main/java/com/awst/androidservicetrain/MusicService.kt
+++ app/src/main/java/com/awst/androidservicetrain/MusicService.kt
@@ -2,9 +2,14 @@ package com.awst.androidservicetrain
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
+import android.util.Log
 
 internal class MusicService : Service() {
+
+    private var mediaPlayer: MediaPlayer? = null
+
     /*
     Наследование от Service заставляет нас переопределить метод onBind.
     Поговорим о нём, когда будем реализовывать bound-сервис.
@@ -12,4 +17,25 @@ internal class MusicService : Service() {
      */
     override fun onBind(intent: Intent?): IBinder? = null
 
+    // Инициализация ресурсов
+    override fun onCreate() {
+        super.onCreate()
+
+        Log.d(LOG_TAG, "onCreate")
+        mediaPlayer = MediaPlayer()
+    }
+
+    // Освобождение ресурсов
+    override fun onDestroy() {
+        Log.d(LOG_TAG, "onDestroy")
+
+        mediaPlayer?.setOnPreparedListener(null)
+        mediaPlayer?.setOnCompletionListener(null)
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    private companion object {
+        const val LOG_TAG = "MusicService"
+    }
 }
