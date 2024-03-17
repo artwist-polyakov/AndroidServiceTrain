0def1721fa58b36abc0b3670268bcefbb8a684e8
Aleksandr Poliakov <master@artwist.ru>
2024-03-17 18:32:35 +0300

    запускает сервис, но  музыка не работает

--- app/src/main/java/com/awst/androidservicetrain/MusicService.kt
+++ app/src/main/java/com/awst/androidservicetrain/MusicService.kt
@@ -6,11 +6,13 @@ import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.media.MediaPlayer
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 
 internal class MusicService : Service() {
 
@@ -54,7 +56,21 @@ internal class MusicService : Service() {
 
         Log.d(LOG_TAG, "onCreate")
         createNotificationChannel()
+        ServiceCompat.startForeground(
+            /* service = */ this,
+            /* id = */ SERVICE_NOTIFICATION_ID,
+            /* notification = */ createServiceNotification(),
+            /* foregroundServiceType = */ getForegroundServiceTypeConstant()
+        )
         mediaPlayer = MediaPlayer()
+    }
+
+    private fun getForegroundServiceTypeConstant(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
+        } else {
+            0
+        }
     }
 
     private fun createNotificationChannel() {
@@ -98,6 +114,7 @@ internal class MusicService : Service() {
 
     private companion object {
         const val LOG_TAG = "MusicService"
+        const val SERVICE_NOTIFICATION_ID = 100
         const val NOTIFICATION_CHANNEL_ID = "music_service_channel"
     }
 }
